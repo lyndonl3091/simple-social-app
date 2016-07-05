e49ee0b3c3407d0e4c020874df36c7e5c45c220b
lyndonl3091 <lyndonl3091@gmail.com>
2016-07-05 16:11:23 -0700

    first commit

---
+++ routes/users.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const express = require('express');
+const User = require('../models/user');
+
+let router = express.Router();
+
+
+
+router.get('/', (req, res) => {
+  User.find({}, (err, users) => {
+    if(err) return res.status(400).send(err);
+  })
+})
+
+router.get('/profile', User.authMiddleware, (req, res) => {
+  res.send(req.user);
+})
+
+router.post('/register', (req, res) => {
+  User.register(req.body, err => {
+    res.status(err ? 400:200).send(err);
+  })
+});
+
+router.post('/login', (req, res) => {
+
+  User.authenticate(req.body, (err, user) => {
+    if(err) return res.status(400).send(err);
+
+    let token = user.generateToken();
+
+    res.cookie('authtoken', token).send(user)
+  })
+})
+
+res.post('/logout', (req, res) => {
+  res.clearCookie('authtoken').send();
+});
